6f67f4191f867a53045d6670f434b1e8e2d2bfa6
Tom Otero <tomotero1984@gmail.com>
2021-05-31 18:48:18 -0700

    adding contact section

--- js/jquery-content-loader.js
+++ js/jquery-content-loader.js
@@ -1,15 +1,32 @@
 // About
-$(function(){
-    $("#about").load("content/about.html")
+$(document).ready(function () {
+    $("#about").load("content/about.html");
 });
 
 // Projects
-$(function(){
-    $("#projects").load("content/projects.html")
+$(document).ready(function () {
+    $("#projects").load("content/projects.html");
 });
 
+// Contact
+// $(document).ready(function () {
+//     $("#contact").load("content/contact.html");
+// });
+
+// Check for element
+// $(document).ready(function () {
+//     $("#contact").on(" load", function () {
+//         if ($("#email").length) {
+//             console.log("Email Found")
+//         }
+//         else {
+//             console.log("Email Missing")
+//             console.log(`${$("#email").length}`)
+//         }
+//     });
+// });
 
 // GetJSON
-$.getJSON("../data/engineering_quotes.json", function(json){
+$.getJSON("../data/engineering_quotes.json", function (json) {
     console.log(json);
 })
